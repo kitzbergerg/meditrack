ffe5c3626458661d31f741fef7367bd5e13378cb
Alina <alina.grassauer25@gmail.com>
2024-05-06 13:07:39 +0200

    feature: #68: add testing for getting user data

--- frontend/src/app/components/department-manager-dashboard/department-manager-dashboard.component.ts
+++ frontend/src/app/components/department-manager-dashboard/department-manager-dashboard.component.ts
@@ -11,33 +11,36 @@ import {HttpClient, HttpHeaders} from "@angular/common/http";
 export class DepartmentManagerDashboardComponent {
 
   private apiUrl = 'http://localhost:8081/api/user';
+  private userinfoUrl = 'http://localhost:8080/realms/meditrack/protocol/openid-connect/userinfo';
+  private accessToken = ''; // Replace with your actual Access Token
   data: any;
+  userinfo: any;
   constructor(private authorizationService: AuthorizationService, private http: HttpClient) {
   }
 
   ngOnInit(): void {
+    this.getToken()
     console.log(this.authorizationService.isLoggedIn());
     //console.log(this.authorizationService.userName)
   }
 
   dGet(): void {
-    this.getData().subscribe(
+    this.getUserData().subscribe(
       (response) => {
         this.data = response;
         console.log(this.data)
-        // Handle response data
       },
       (error) => {
         console.error('Error fetching data:', error);
-        // Handle error
       }
     );}
 
-  getData(): Observable<any> {
-    return this.http.get<any>(this.apiUrl);
-  }
 
-  userinfo: any;
+  async getToken() {
+    await this.authorizationService.token.then(r => {
+      this.accessToken = r;
+    })
+  }
 
   async getUser() {
     this.accessToken = await this.authorizationService.token
@@ -47,23 +50,24 @@ export class DepartmentManagerDashboardComponent {
     );
   }
 
-
   getUserName() {
     this.getUser().then(r => {if (this.userinfo != undefined) {
       console.log(this.userinfo.name)
     }})
-
   }
 
-  private userinfoUrl = 'http://localhost:8080/realms/meditrack/protocol/openid-connect/userinfo';
-  private accessToken = ''; // Replace with your actual Access Token
-
+  getUserData(): Observable<any> {
+    console.log(this.accessToken)
+    const headers = new HttpHeaders({
+      'Authorization': `Bearer ${this.accessToken}`
+    });
+    return this.http.get<any>(this.apiUrl, { headers });
+  }
 
   getUserKeycloakInfo() {
     const headers = new HttpHeaders({
       'Authorization': `Bearer ${this.accessToken}`
     });
-
     return this.http.get<any>(this.userinfoUrl, { headers });
   }
 
