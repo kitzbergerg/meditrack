b08b6d9a368029bd8658c062d07b84deaab3a0f2
Lucas Wolkersdorfer <wolke2807@gmail.com>
2024-05-15 01:06:25 +0200

    #37: add static mock data in monthly plan creator

--- backend/src/main/java/ase/meditrack/service/algorithm/MonthlyPlanCreator.java
+++ backend/src/main/java/ase/meditrack/service/algorithm/MonthlyPlanCreator.java
@@ -1,11 +1,18 @@
 package ase.meditrack.service.algorithm;
 
+import ase.meditrack.model.entity.HardConstraints;
+import ase.meditrack.model.entity.MonthlyPlan;
 import ase.meditrack.model.entity.Shift;
+import ase.meditrack.model.entity.ShiftType;
+import ase.meditrack.model.entity.Team;
 import ase.meditrack.model.entity.User;
-import ase.meditrack.repository.UserRepository;
+import ase.meditrack.repository.MonthlyPlanRepository;
+import ase.meditrack.repository.TeamRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.time.LocalTime;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
@@ -14,39 +21,94 @@ import java.util.UUID;
 public class MonthlyPlanCreator {
 
     private AlgorithmMapper algorithmMapper;
-    private UserRepository userRepository;
+    private TeamRepository teamRepository;
+    private MonthlyPlanRepository monthlyPlanRepository;
 
     @Autowired
-    public MonthlyPlanCreator(AlgorithmMapper algorithmMapper, UserRepository userRepository) {
-        this.algorithmMapper = algorithmMapper;
-        this.userRepository = userRepository;
+    public MonthlyPlanCreator(TeamRepository teamRepository, MonthlyPlanRepository monthlyPlanRepository) {
+        this.teamRepository = teamRepository;
+        this.monthlyPlanRepository = monthlyPlanRepository;
     }
 
-    public void createMonthlyPlan(int month, int year, UUID team) {
+    public MonthlyPlan createMonthlyPlan(int month, int year, UUID teamId) {
+
+        // Create mock data
+        // TODO: Replace mocked data with db fetch
+        Team team = new Team();
+        team.setId(teamId);
+        team.setWorkingHours(40);
+        team.setName("Mock Team");
+
+        HardConstraints constraints = new HardConstraints();
+        constraints.setAllowedFlextimePerMonth(20);
+        constraints.setAllowedFlextimeTotal(20);
+        constraints.setDaytimeRequiredPeople(3);
+        constraints.setNighttimeRequiredPeople(2);
+
+        List<ShiftType> shiftTypes = new ArrayList<>();
+        for (int i = 0; i < 3; i++) {
+            ShiftType shiftType = new ShiftType();
+            shiftType.setId(UUID.randomUUID());
+            shiftType.setStartTime(LocalTime.of(9 + i * 3, 0));
+            shiftType.setEndTime(LocalTime.of(12 + i * 3, 0));
+            shiftTypes.add(shiftType);
+        }
+
+        List<User> users = new ArrayList<>();
+        for (int i = 0; i < 3; i++) {
+            User user = new User();
+            user.setId(UUID.randomUUID());
+            user.setCanWorkShiftTypes(shiftTypes);
+            user.setWorkingHoursPercentage(1.0F);
+            user.setCurrentOverTime(0);
+            users.add(user);
+        }
+
+        team.setUsers(users);
+        team.setShiftTypes(shiftTypes);
+        team.setHardConstraints(new HardConstraints());
+
         // TODO: fetch from db
+        /*
+        Optional<Team> team = teamRepository.findById(teamId);
 
-        List<User> users = userRepository.findAll();
+        if (team.isEmpty()) {
+            throw new RuntimeException("Could not find team");
+        }
 
-        // TODO: map to algorithm input
 
-        AlgorithmInput input = algorithmMapper.mapToAlgorithmInput(month, year, users, null, null);
+
+        List<User> users = team.get().getUsers();
+        HardConstraints constraints = team.get().getHardConstraints();
+        List<ShiftType> shiftTypes = team.get().getShiftTypes();
+         */
+
+        // map to algorithm input
+        algorithmMapper = new AlgorithmMapper();
+
+        AlgorithmInput input = algorithmMapper.mapToAlgorithmInput(month, year, users, shiftTypes, constraints, team);
 
         Optional<AlgorithmOutput> output = SchedulingSolver.solve(input);
 
-        // TODO: map from algorithm output
-
+        // map from algorithm output
         if(output.isEmpty()) {
-            throw new RuntimeException("Couldn't generate schedule");
+            throw new RuntimeException("Could not generate schedule");
         }
 
-        List<Shift> shifts = algorithmMapper.mapFromAlgorithmOutput(output.get());
+        MonthlyPlan monthlyPlan = new MonthlyPlan();
 
+        List<Shift> shifts = algorithmMapper.mapFromAlgorithmOutput(output.get(), shiftTypes, users, monthlyPlan, month, year);
 
         // TODO: create shifts and monthly plan
-    }
 
-    private boolean persistShifts(){
-        return false;
+        monthlyPlan.setMonth(month);
+        monthlyPlan.setYear(year);
+        monthlyPlan.setTeam(team);
+        monthlyPlan.setPublished(false);
+        monthlyPlan.setShifts(shifts);
+
+        // monthlyPlanRepository.save(monthlyPlan);
+        return monthlyPlan;
     }
 
 }
