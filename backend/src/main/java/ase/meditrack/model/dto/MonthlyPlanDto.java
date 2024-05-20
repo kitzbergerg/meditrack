829ac8bfd04b81834dd02a25ea21f56896d74f35
Jakob Petermandl <e11776823@student.tuwien.ac.at>
2024-05-20 18:46:42 +0200

    #70: remove hardcoded year and use year instead of integer in dto

--- backend/src/main/java/ase/meditrack/model/dto/MonthlyPlanDto.java
+++ backend/src/main/java/ase/meditrack/model/dto/MonthlyPlanDto.java
@@ -2,24 +2,24 @@ package ase.meditrack.model.dto;
 
 import ase.meditrack.model.CreateValidator;
 import ase.meditrack.model.UpdateValidator;
-import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.FutureOrPresent;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Null;
 import org.hibernate.validator.constraints.Range;
 
+import java.time.Year;
 import java.util.List;
 import java.util.UUID;
 
 public record MonthlyPlanDto(
         @Null(groups = CreateValidator.class) @NotNull(groups = UpdateValidator.class) UUID id,
         @NotNull(groups = CreateValidator.class) @Range(min = MIN_MONTH, max = MAX_MONTH) Integer month,
-        @NotNull(groups = CreateValidator.class) @Min(MIN_YEAR) Integer year,
+        @NotNull(groups = CreateValidator.class) @FutureOrPresent Year year,
         @NotNull(groups = CreateValidator.class) Boolean published,
         @NotNull(groups = CreateValidator.class) UUID team,
         List<UUID> shifts
 ) {
     private static final int MIN_MONTH = 1;
     private static final int MAX_MONTH = 12;
-    private static final int MIN_YEAR = 2024;
 }
 
--- backend/src/main/java/ase/meditrack/model/mapper/MonthlyPlanMapper.java
+++ backend/src/main/java/ase/meditrack/model/mapper/MonthlyPlanMapper.java
@@ -4,6 +4,7 @@ import ase.meditrack.model.dto.MonthlyPlanDto;
 import ase.meditrack.model.entity.MonthlyPlan;
 import org.mapstruct.IterableMapping;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.Named;
 
 import java.util.List;
@@ -12,8 +13,10 @@ import java.util.List;
 public interface MonthlyPlanMapper {
 
     @Named("toDto")
+    @Mapping(target = "year", expression = "java(Year.of(monthlyPlan.getYear()))")
     MonthlyPlanDto toDto(MonthlyPlan monthlyPlan);
 
+    @Mapping(target = "year", expression = "java(dto.year().getValue())")
     MonthlyPlan fromDto(MonthlyPlanDto dto);
 
     @IterableMapping(qualifiedByName = "toDto")
