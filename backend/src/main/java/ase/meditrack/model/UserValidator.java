fc5d863e6279b671a20e08c72cdd584bd1e4f8da
kitzbergerg <gabriel.kitzberger@gmail.com>
2024-05-22 13:12:49 +0200

    #37: remove manual logger and unused realmresource

--- backend/src/main/java/ase/meditrack/model/UserValidator.java
+++ backend/src/main/java/ase/meditrack/model/UserValidator.java
@@ -4,14 +4,10 @@ import ase.meditrack.exception.NotFoundException;
 import ase.meditrack.model.entity.User;
 import ase.meditrack.repository.UserRepository;
 import lombok.extern.slf4j.Slf4j;
-import org.keycloak.admin.client.resource.RealmResource;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ResponseStatusException;
 
-import java.lang.invoke.MethodHandles;
 import java.security.Principal;
 import java.util.Optional;
 import java.util.UUID;
@@ -19,17 +15,14 @@ import java.util.UUID;
 @Component
 @Slf4j
 public class UserValidator {
-    private static final Logger LOGGER = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
-    private final RealmResource meditrackRealm;
     private UserRepository userRepository;
 
-    public UserValidator(UserRepository userRepository, RealmResource meditrackRealm) {
+    public UserValidator(UserRepository userRepository) {
         this.userRepository = userRepository;
-        this.meditrackRealm = meditrackRealm;
     }
 
     public void teamValidate(UUID userId, Principal principal) {
-        LOGGER.trace("Validate if team of dm {} and user {} match.", userId, principal.getName());
+        log.trace("Validate if team of dm {} and user {} match.", userId, principal.getName());
         Optional<User> dm = userRepository.findById(UUID.fromString(principal.getName()));
         if (dm.isEmpty()) {
             throw new NotFoundException("DM account could not be found");
