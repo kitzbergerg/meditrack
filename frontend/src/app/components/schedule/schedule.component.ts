b726e3b6b545818baf1f9e992938cf0bc5fbb2ef
Lucas Wolkersdorfer <wolke2807@gmail.com>
2024-05-23 03:48:34 +0200

    #37: move data loading and parsing to schedule component

--- frontend/src/app/components/schedule/schedule.component.ts
+++ frontend/src/app/components/schedule/schedule.component.ts
@@ -1,17 +1,77 @@
-import { Component } from '@angular/core';
-import {HeaderComponent} from "./header/header.component";
+import {Component, OnInit} from '@angular/core';
 import {WeekViewComponent} from "./week-view/week-view.component";
+import {Day} from "../../interfaces/schedule.models";
+import {ScheduleService} from "../../services/schedule.service";
 
 @Component({
   selector: 'app-schedule',
   standalone: true,
   imports: [
-    HeaderComponent,
     WeekViewComponent
   ],
   templateUrl: './schedule.component.html',
   styleUrl: './schedule.component.scss'
 })
-export class ScheduleComponent {
+export class ScheduleComponent implements OnInit {
+
+  allDays: Day[] = [];
+  days: Day[] = [];
+  employees: any[] = [];
+  currentWeekOffset = 0;
+  startDate: Date = new Date();
+
+  constructor(private scheduleService: ScheduleService) {
+  }
+
+  ngOnInit(): void {
+    this.loadSchedule();
+  }
+
+  loadSchedule(): void {
+    this.scheduleService.getSchedule().subscribe(data => {
+      this.allDays = data.days;
+      this.startDate = new Date(data.year, data.month, 1);
+      this.loadWeek();
+    });
+  }
+
+  loadWeek(): void {
+    const start = this.currentWeekOffset * 7;
+    this.days = this.allDays.slice(start, start + 7);
+    this.transformData(this.days);
+  }
+
+  transformData(days: Day[]): void {
+    const employeeMap = new Map<string, any>();
+
+    days.forEach(day => {
+      day.dayName = this.getDayString(day.day - (this.currentWeekOffset * 7));
+      day.shifts.forEach(shift => {
+        const name = `${shift.employee.firstname} ${shift.employee.lastname}`;
+        const role = shift.employee.role;
+        const workingPercentage = `${shift.employee.working_percentage * 100}`;
+        if (!employeeMap.has(name)) {
+          employeeMap.set(name, {name, role, workingPercentage, shifts: {}});
+        }
+        employeeMap.get(name).shifts[day.day] = shift;
+      });
+    });
+
+    this.employees = Array.from(employeeMap.values());
+  }
+
+  getDayString(dayIndex: number): string {
+    const date = new Date(this.startDate);
+    date.setDate(this.startDate.getDate() + dayIndex - 1);
+    const dayString = date.toLocaleDateString('en-GB', {day: '2-digit', month: '2-digit'});
+    const dayOfWeekString = date.toLocaleDateString('en-GB', {weekday: 'short'});
+    return `${dayString} ${dayOfWeekString}`;
+  }
+
+  changeWeek(offset: number): void {
+    this.currentWeekOffset += offset;
+    this.startDate.setDate(this.startDate.getDate() + (offset * 7));
+    this.loadWeek();
+  }
 
 }
