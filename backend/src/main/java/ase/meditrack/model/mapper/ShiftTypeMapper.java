769f01dca201e55ec596590e1ab1c98919a27692
kitzbergerg <gabriel.kitzberger@gmail.com>
2024-05-25 23:00:06 +0200

    #37: clean up mappers

--- backend/src/main/java/ase/meditrack/model/mapper/ShiftTypeMapper.java
+++ backend/src/main/java/ase/meditrack/model/mapper/ShiftTypeMapper.java
@@ -5,7 +5,6 @@ import ase.meditrack.model.dto.SimpleShiftTypeDto;
 import ase.meditrack.model.entity.ShiftType;
 import org.mapstruct.IterableMapping;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 import org.mapstruct.Named;
 
 import java.util.List;
@@ -19,16 +18,6 @@ public interface ShiftTypeMapper {
     ShiftType fromDto(ShiftTypeDto dto);
 
     @Named("toSimpleDto")
-    @Mapping(target = "id", source = "canWorkShiftType.id")
-    @Mapping(target = "name", source = "canWorkShiftType.name")
-    @Mapping(target = "startTime", source = "canWorkShiftType.startTime")
-    @Mapping(target = "endTime", source = "canWorkShiftType.endTime")
-    @Mapping(target = "breakStartTime", source = "canWorkShiftType.breakStartTime")
-    @Mapping(target = "breakEndTime", source = "canWorkShiftType.breakEndTime")
-    @Mapping(target = "type", source = "canWorkShiftType.type")
-    @Mapping(target = "color", source = "canWorkShiftType.color")
-    @Mapping(target = "abbreviation", source = "canWorkShiftType.abbreviation")
-    @Mapping(target = "team", source = "canWorkShiftType.team")
     SimpleShiftTypeDto toSimpleDto(ShiftType canWorkShiftType);
 
     @IterableMapping(qualifiedByName = "toSimpleDto")
