b6ef93b198548079c413b0201071f3184375cafb
kitzbergerg <gabriel.kitzberger@gmail.com>
2024-05-26 11:17:48 +0200

    #78: fix remaining checkstyle errors and require checkstyle to pass in pipeline

--- backend/src/main/java/ase/meditrack/model/UserValidator.java
+++ backend/src/main/java/ase/meditrack/model/UserValidator.java
@@ -9,7 +9,6 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.security.Principal;
-import java.util.Optional;
 import java.util.UUID;
 
 @Component
@@ -21,17 +20,21 @@ public class UserValidator {
         this.userRepository = userRepository;
     }
 
+    /**
+     * Checks if the user is allowed to edit the team.
+     *
+     * @param userId    the user that will be edited
+     * @param principal the current user
+     */
     public void teamValidate(UUID userId, Principal principal) {
         log.trace("Validate if team of dm {} and user {} match.", userId, principal.getName());
-        Optional<User> dm = userRepository.findById(UUID.fromString(principal.getName()));
-        if (dm.isEmpty()) {
-            throw new NotFoundException("DM account could not be found");
-        }
-        Optional<User> user = userRepository.findById(userId);
-        if (user.isEmpty()) {
-            throw new NotFoundException("User to delete could not be found");
-        }
-        if (!user.get().getTeam().equals(dm.get().getTeam())) {
+        User dm = userRepository.findById(UUID.fromString(principal.getName()))
+                .orElseThrow(() -> new NotFoundException("DM account could not be found"));
+
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new NotFoundException("User to delete could not be found"));
+
+        if (!user.getTeam().equals(dm.getTeam())) {
             throw new ResponseStatusException(HttpStatus.UNAUTHORIZED,
                     "You are not authorized to modify an account that is not part of your team");
         }
