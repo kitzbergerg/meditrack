0916478f16a7b218444839712b13bda319d66ea6
Luca Marius Cobzaru <e11712226@student.tuwien.ac.at>
2024-05-26 14:27:08 +0200

    dto and mapper

--- backend/src/main/java/ase/meditrack/model/mapper/HardConstraintsMapper.java
+++ backend/src/main/java/ase/meditrack/model/mapper/HardConstraintsMapper.java
@@ -3,11 +3,14 @@ package ase.meditrack.model.mapper;
 import ase.meditrack.model.dto.HardConstraintsDto;
 import ase.meditrack.model.entity.HardConstraints;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 
 @Mapper(uses = EntityUuidMapper.class)
 public interface HardConstraintsMapper {
+    @Mapping(target = "shiftOffShift", ignore = true)
     HardConstraintsDto toDto(HardConstraints hardConstraints);
 
+    @Mapping(target = "shiftOffShift", ignore = true)
     HardConstraints fromDto(HardConstraintsDto dto);
 }
