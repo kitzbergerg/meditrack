19f85b45a9775882f9e3ea79e1bd437436c542ef
Lucas Wolkersdorfer <wolke2807@gmail.com>
2024-05-31 23:00:27 +0200

    #39: create delete schedule in schedule service

--- frontend/src/app/services/schedule.service.ts
+++ frontend/src/app/services/schedule.service.ts
@@ -15,7 +15,6 @@ export class ScheduleService {
   private baseUrl = 'http://localhost:8081/api/monthly-plan';
 
   createSchedule(month: string, year: number): Observable<Schedule> {
-    //TODO: param should not be required in backend
     const params = new HttpParams()
       .set('year', year.toString())
       .set('month', month);
@@ -25,13 +24,17 @@ export class ScheduleService {
   }
 
   fetchSchedule(month: string, year: number): Observable<Schedule> {
-    //TODO: param should not be required in backend
     const params = new HttpParams()
       .set('year', year.toString())
       .set('month', month);
 
     const url = `${this.baseUrl}/team`;
     return this.http.get<Schedule>(url, {params});
+  }
+
+  deleteSchedule(scheduleId: string) {
+    const url = `${this.baseUrl}/${scheduleId}`;
+    return this.http.delete(url);
   }
 
 }
