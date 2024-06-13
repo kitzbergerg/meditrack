f5ec839ad6c68b54e62dc9416e6d607c6e347790
Jakob Petermandl <e11776823@student.tuwien.ac.at>
2024-06-13 22:10:09 +0200

    #50: create holiday service and components, implement service

---
+++ frontend/src/app/components/holidays/holidays.component.ts
@@ -0,0 +1,12 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-holidays',
+  standalone: true,
+  imports: [],
+  templateUrl: './holidays.component.html',
+  styleUrl: './holidays.component.scss'
+})
+export class HolidaysComponent {
+
+}
---
+++ frontend/src/app/interfaces/holiday.ts
@@ -0,0 +1,14 @@
+export interface Holiday {
+  id?: string;
+  startDate: string;
+  endDate: string;
+  user: string;
+  status: HolidayRequestStatus
+}
+
+export enum HolidayRequestStatus {
+  REQUESTED = 'REQUESTED',
+  APPROVED = 'APPROVED',
+  CANCELLED = 'CANCELLED',
+  REJECTED = 'REJECTED'
+}
