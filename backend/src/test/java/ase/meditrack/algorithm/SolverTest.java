5244327212f9c782c89415ee9984259eaa8709ea
kitzbergerg <gabriel.kitzberger@gmail.com>
2024-06-14 15:46:49 +0200

    #86: add test for required people

--- backend/src/test/java/ase/meditrack/algorithm/SolverTest.java
+++ backend/src/test/java/ase/meditrack/algorithm/SolverTest.java
@@ -77,4 +77,22 @@ class SolverTest {
         assertTrue(SchedulingSolver.solve(input).isPresent());
     }
 
+    @Test
+    void testRequiredPeople() {
+        // employee can only work from 8-16; 16-20 is therefore empty -> no solution
+        List<EmployeeInfo> employeeInfos = new ArrayList<>();
+        employeeInfos.add(new EmployeeInfo(List.of(0), 28 * 8 - 20, 28 * 8 + 20, 28 * 8, Set.of(), Set.of()));
+        List<ShiftTypeInfo> shiftTypeInfos = List.of(
+                new ShiftTypeInfo(LocalTime.of(8, 0), LocalTime.of(16, 0), 8),
+                new ShiftTypeInfo(LocalTime.of(16, 0), LocalTime.of(0, 0), 8)
+        );
+        List<RoleInfo> roles = List.of(new RoleInfo("Rolename", 0, 0));
+        AlgorithmInput input = new AlgorithmInput(28, employeeInfos, shiftTypeInfos, roles, 1, 0);
+        assertTrue(SchedulingSolver.solve(input).isEmpty());
+
+        // second employee can work both shiftType -> solution
+        employeeInfos.add(new EmployeeInfo(List.of(0, 1), 28 * 8 - 20, 28 * 8 + 20, 28 * 8, Set.of(), Set.of()));
+        assertTrue(SchedulingSolver.solve(input).isPresent());
+    }
+
 }
