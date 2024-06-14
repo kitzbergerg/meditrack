ccfe9097da8839bc8ba5673a5b895804de13784e
kitzbergerg <gabriel.kitzberger@gmail.com>
2024-06-14 15:58:12 +0200

    #86: re-add repository annotation

--- backend/src/main/java/ase/meditrack/repository/HolidayRepository.java
+++ backend/src/main/java/ase/meditrack/repository/HolidayRepository.java
@@ -4,11 +4,13 @@ import ase.meditrack.model.entity.Holiday;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
 import java.util.List;
 import java.util.UUID;
 
+@Repository
 public interface HolidayRepository extends JpaRepository<Holiday, UUID> {
 
     /**
