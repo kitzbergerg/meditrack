4b651edc3007b9e10372a5b68ec1213841d19f76
Jakob Petermandl <e11776823@student.tuwien.ac.at>
2024-06-16 17:56:52 +0200

    #50: implement dm view for holidays

--- frontend/src/app/components/holidays/holidays.component.ts
+++ frontend/src/app/components/holidays/holidays.component.ts
@@ -1,8 +1,9 @@
 import {Component} from '@angular/core';
-import {Holiday} from "../../interfaces/holiday";
+import {Holiday, HolidayRequestStatus} from "../../interfaces/holiday";
 import {HolidaysService} from "../../services/holidays.service";
 import {AuthorizationService} from "../../services/authorization/authorization.service";
 import {MessageService} from "primeng/api";
+import {UserService} from "../../services/user.service";
 
 @Component({
   selector: 'app-holidays',
@@ -13,11 +14,17 @@ export class HolidaysComponent {
 
   loading = true;
   holidays: Holiday[] = [];
+  teamHolidays: Holiday[] = [];
+  userIdNameMap: Map<string, string> = new Map<string, string>();
   userId = '';
+  isUserDm = false;
+  loadingTeamHolidays = true;
 
   submitted = false;
   valid = false;
   holiday: Holiday = { id: undefined, startDate: '', endDate: '', status: undefined, user: undefined };
+  teamHoliday: Holiday = { id: undefined, startDate: '', endDate: '', status: undefined, user: undefined };
+  teamUserName: string | undefined = '';
 
   initialLoad = false;
 
@@ -27,12 +34,18 @@ export class HolidaysComponent {
 
   constructor(private holidaysService: HolidaysService,
               private authorizationService: AuthorizationService,
-              private messageService: MessageService
+              private messageService: MessageService,
+              private userService: UserService
   ) { }
 
   ngOnInit() {
     this.userId = this.authorizationService.parsedToken().sub;
     this.getAllHolidaysFromUser();
+    this.isUserDm = this.authorizationService.hasAuthority(["admin", "dm"]);
+    if (this.isUserDm) {
+      this.getAllHolidaysFromTeam();
+      this.userIdNameMap = this.getTeamUsersToMap();
+    }
   }
 
   getAllHolidaysFromUser() {
@@ -96,6 +109,7 @@ export class HolidaysComponent {
   updateHoliday() {
     this.submitted = true;
     if (this.valid) {
+      this.holiday.status = HolidayRequestStatus.REQUESTED;
       this.holidaysService.updateHoliday(this.holiday)
         .subscribe({
           next: (response) => {
@@ -103,6 +117,9 @@ export class HolidaysComponent {
             this.messageService.add({severity: 'success', summary: 'Success', detail: 'Holiday updated successfully'});
             this.selectHoliday(this.holiday);
             this.resetForm();
+            if (this.isUserDm) {
+              this.getAllHolidaysFromTeam();
+            }
           }, error: (error) => {
             this.messageService.add({severity: 'error', summary: 'Updating holiday failed', detail: error.error});
           }
@@ -174,4 +191,84 @@ export class HolidaysComponent {
     this.resetForm();
   }
 
+  getAllHolidaysFromTeam() {
+    this.holidaysService.getAllHolidaysByTeam()
+      .subscribe({
+        next: (response) => {
+          this.teamHolidays = response
+            .sort((a, b) => Date.parse(a.startDate) - Date.parse(b.startDate));
+          this.loadingTeamHolidays = false;
+          this.selectTeamHoliday(this.findFirstRequestedHoliday());
+        }, error: (error) => {
+          console.error('Error fetching data:', error);
+        }
+      })
+  }
+
+  selectTeamHoliday(holiday: Holiday | undefined) {
+    if (holiday === undefined) {
+      return;
+    }
+    this.teamHoliday = holiday;
+    if (holiday.user != undefined) {
+      this.teamUserName = this.getUsernameFromId(holiday.user);
+    }
+  }
+
+  getTeamUsersToMap() {
+    const map = new Map<string, string>();
+    this.userService.getAllUserFromTeam()
+      .subscribe({
+        next: (response) => {
+          response.forEach(user => {
+            if (user.id != undefined && user.username != undefined) {
+              map.set(user.id, user.username);
+            }
+          });
+          this.selectTeamHoliday(this.findFirstRequestedHoliday());
+        }, error: (error) => {
+          console.error('Error fetching data:', error);
+        }
+      });
+    return map;
+  }
+
+  findFirstRequestedHoliday() {
+    return this.teamHolidays.find(holiday => holiday.status === HolidayRequestStatus.REQUESTED);
+  }
+
+  getUsernameFromId(id: string) {
+    if (this.userIdNameMap.has(id)) {
+      return this.userIdNameMap.get(id);
+    }
+    return '';
+  }
+
+  updateStatus(status: string) {
+    if (this.teamHoliday.id === undefined) {
+      this.messageService.add({severity: 'error', summary: 'Error', detail: 'Status of holiday could not be updated!'});
+      return;
+    }
+    this.holidaysService.updateHolidayStatus(this.teamHoliday.id, status)
+      .subscribe({
+        next: (response) => {
+          console.log('Holiday status updated successfully:', response);
+          this.messageService.add({severity: 'success', summary: 'Success', detail: 'Holiday status updated successfully'});
+          this.getAllHolidaysFromTeam();
+          this.getAllHolidaysFromUser(); // Refresh user holidays because the dm can also approve his own holidays
+        }, error: (error) => {
+          this.messageService.add({severity: 'error', summary: 'Updating holiday failed', detail: error.error});
+        }
+      });
+  }
+
+  isApprovable() {
+    return this.teamHoliday.status === HolidayRequestStatus.REQUESTED;
+  }
+
+  isCancelable() {
+    return this.teamHoliday.status === HolidayRequestStatus.APPROVED;
+  }
+
+  protected readonly HolidayRequestStatus = HolidayRequestStatus;
 }
