aa9a33ff2d67f59937ccaf481e4dd190906cee23
Lucas Wolkersdorfer <e11922587@student.tuwien.ac.at>
2024-06-18 17:55:18 +0200

    #80 update monthly plan service tests

--- backend/src/test/java/ase/meditrack/service/MonthlyPlanServiceTest.java
+++ backend/src/test/java/ase/meditrack/service/MonthlyPlanServiceTest.java
@@ -1,6 +1,7 @@
 package ase.meditrack.service;
 
 import ase.meditrack.config.KeycloakConfig;
+import ase.meditrack.exception.NotFoundException;
 import ase.meditrack.model.entity.MonthlyPlan;
 import ase.meditrack.model.entity.Shift;
 import ase.meditrack.model.entity.Team;
@@ -13,6 +14,11 @@ import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.keycloak.admin.client.resource.RealmResource;
+import org.keycloak.admin.client.resource.UserResource;
+import org.keycloak.admin.client.resource.UsersResource;
+import org.keycloak.representations.idm.UserRepresentation;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -23,13 +29,16 @@ import org.testcontainers.junit.jupiter.Testcontainers;
 
 import java.security.Principal;
 import java.time.LocalDate;
+import java.time.Month;
+import java.time.Year;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertAll;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 @Transactional
 @SpringBootTest
@@ -41,19 +50,28 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 @MockBean(RealmResource.class)
 public class MonthlyPlanServiceTest {
     private static final String USER_ID = "00000000-0000-0000-0000-000000000000";
+    private User user;
+    private Team team;
 
     @Autowired
     private MonthlyPlanRepository repository;
     @Autowired
-    private MonthlyPlanService service;
-    @Autowired
     private ShiftRepository shiftRepository;
     @Autowired
     private UserRepository userRepository;
-    private User user;
+    @MockBean
+    private UserService userService;
+
     @Autowired
     private TeamService teamService;
-    private Team team;
+    @MockBean
+    RealmResource realmResource;
+    @Mock
+    private RealmResource meditrackRealm;
+    @Mock
+    private UsersResource usersResource;
+    @Autowired
+    private MonthlyPlanService service;
 
     @BeforeEach
     void setup() {
@@ -76,6 +94,7 @@ public class MonthlyPlanServiceTest {
                 null,
                 null
         ));
+
         team = teamService.create(
                 new Team(null, "test team", 40, null, null, null, null, null),
                 () -> USER_ID
@@ -112,30 +131,126 @@ public class MonthlyPlanServiceTest {
     }
 
     @Test
-    void getMonthlyPlan() {
+    void publishMonthlyPlan() {
+        MonthlyPlan monthlyPlan = new MonthlyPlan();
+        monthlyPlan.setTeam(team);
+        monthlyPlan.setYear(2024);
+        monthlyPlan.setMonth(6);
+        monthlyPlan.setPublished(false);
+        repository.save(monthlyPlan);
+
+        service.publish(monthlyPlan.getId(), () -> USER_ID);
+
+        MonthlyPlan savedMonthlyPlan = repository.findById(monthlyPlan.getId()).get();
+
+        MonthlyPlan result = service.findById(savedMonthlyPlan.getId());
+
+        assertAll(
+                () -> assertNotNull(result),
+                () -> assertEquals(monthlyPlan.getId(), result.getId()),
+                () -> assertTrue(monthlyPlan.getPublished())
+        );
+    }
+
+    @Test
+    void isPublishedMonthlyPlan() {
+        MonthlyPlan monthlyPlan = new MonthlyPlan();
+        monthlyPlan.setTeam(team);
+        monthlyPlan.setYear(2024);
+        monthlyPlan.setMonth(6);
+        monthlyPlan.setPublished(false);
+        repository.save(monthlyPlan);
+
+        when(userService.getPrincipalWithTeam(any())).thenReturn(user);
+
+        assertFalse(service.isPublished(Month.JUNE, Year.of(2024), () -> USER_ID));
+
+        service.publish(monthlyPlan.getId(), () -> USER_ID);
+
+        assertTrue(service.isPublished(Month.JUNE, Year.of(2024), () -> USER_ID));
+    }
+
+    @Test
+    void isUserInTeamOfMonthlyPlan() {
+        MonthlyPlan monthlyPlan = new MonthlyPlan();
+        monthlyPlan.setTeam(new Team());
+        monthlyPlan.setYear(2024);
+        monthlyPlan.setMonth(6);
+        monthlyPlan.setPublished(false);
+        repository.save(monthlyPlan);
+        when(userService.findById(any())).thenReturn(user);
+
+        assertFalse(service.isUserInTeam(user.getId(), monthlyPlan.getId()));
+
+        monthlyPlan.setTeam(team);
+
+        assertTrue(service.isUserInTeam(user.getId(), monthlyPlan.getId()));
+    }
+
+    @Test
+    void isShiftFromTeam() {
         MonthlyPlan monthlyPlan = new MonthlyPlan();
         monthlyPlan.setTeam(team);
         monthlyPlan.setYear(2024);
         monthlyPlan.setMonth(6);
         monthlyPlan.setPublished(false);
         Shift shift = new Shift();
+        shift.setUsers(List.of(user));
+        shift.setMonthlyPlan(monthlyPlan);
         shift.setDate(LocalDate.now().plusDays(2));
-        shiftRepository.save(shift);
-        Shift shift2 = new Shift();
-        shift.setDate(LocalDate.now().plusDays(3));
-        shiftRepository.save(shift2);
+        shift = shiftRepository.save(shift);
         List<Shift> shifts = new ArrayList<>();
         shifts.add(shift);
-        shifts.add(shift2);
         monthlyPlan.setShifts(shifts);
         repository.save(monthlyPlan);
 
-        Principal principal = new Principal() {
-            @Override
-            public String getName() {
-                return USER_ID;
-            }
-        };
+        when(userService.findById(any())).thenReturn(user);
+
+        assertTrue(service.isShiftFromTeam(user.getId(), shift.getId()));
+    }
+
+    @Test
+    void getMonthlyPlanNotFound() {
+        MonthlyPlan monthlyPlan = new MonthlyPlan();
+        monthlyPlan.setTeam(team);
+        monthlyPlan.setYear(2024);
+        monthlyPlan.setMonth(6);
+        monthlyPlan.setPublished(false);
+
+        repository.save(monthlyPlan);
+
+        when(userService.findById(any())).thenReturn(user);
+        Principal principal = () -> USER_ID;
+
+        assertThrows(NotFoundException.class, () -> service.getMonthlyPlan(7, 2024, principal));
+    }
+
+    @Test
+    void getMonthlyPlan() {
+        MonthlyPlan monthlyPlan = new MonthlyPlan();
+        monthlyPlan.setTeam(team);
+        monthlyPlan.setYear(2024);
+        monthlyPlan.setMonth(6);
+        monthlyPlan.setPublished(false);
+
+        Shift shift1 = new Shift();
+        shift1.setUsers(List.of(user));
+        shift1.setDate(LocalDate.now().plusDays(2));
+        Shift shift2 = new Shift();
+        shift2.setUsers(List.of(user));
+        shift2.setDate(LocalDate.now().plusDays(3));
+        List<Shift> shifts = new ArrayList<>();
+        shifts.add(shift1);
+        shifts.add(shift2);
+        monthlyPlan.setShifts(shifts);
+
+        repository.save(monthlyPlan);
+
+        when(userService.findById(any())).thenReturn(user);
+        when(usersResource.get(any())).thenReturn(mock(UserResource.class));
+        when(realmResource.users()).thenReturn(usersResource);
+        Principal principal = () -> USER_ID;
+
         MonthlyPlan savedMonthlyPlan = service.getMonthlyPlan(6, 2024, principal);
 
         assertAll(
