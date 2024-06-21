3452eefc5a3e135d1e308cca866d01c9533a5fcd
Jakob Petermandl <e11776823@student.tuwien.ac.at>
2024-06-21 22:28:10 +0200

    #50: tests

--- backend/src/test/java/ase/meditrack/controller/HolidayControllerIT.java
+++ backend/src/test/java/ase/meditrack/controller/HolidayControllerIT.java
@@ -3,7 +3,6 @@ package ase.meditrack.controller;
 import ase.meditrack.config.KeycloakConfig;
 import ase.meditrack.model.dto.HolidayDto;
 import ase.meditrack.model.entity.Holiday;
-import ase.meditrack.model.entity.Preferences;
 import ase.meditrack.model.entity.User;
 import ase.meditrack.model.entity.enums.HolidayRequestStatus;
 import ase.meditrack.repository.HolidayRepository;
@@ -11,7 +10,6 @@ import ase.meditrack.repository.UserRepository;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.keycloak.admin.client.resource.RealmResource;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -27,14 +25,12 @@ import org.testcontainers.junit.jupiter.Testcontainers;
 
 import java.time.LocalDate;
 import java.util.List;
-import java.util.Objects;
 import java.util.UUID;
 
-import static org.junit.jupiter.api.Assertions.assertAll;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
@@ -44,23 +40,26 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @MockBean(KeycloakConfig.class)
 @MockBean(KeycloakConfig.KeycloakPostConstruct.class)
 @MockBean(RealmResource.class)
-@Disabled //disabled for now, because cannot mock keycloak properly
 class HolidayControllerIT {
     private static final String USER_ID = "00000000-0000-0000-0000-000000000000";
 
     @Autowired
     private MockMvc mockMvc;
+
     @Autowired
     private ObjectMapper objectMapper;
+
     @Autowired
     private HolidayRepository holidayRepository;
+
     @Autowired
     private UserRepository userRepository;
+
     private User user;
 
     @BeforeEach
     void setup() {
-        User user = new User(
+        user = userRepository.save(new User(
                 UUID.fromString(USER_ID),
                 null,
                 1f,
@@ -76,17 +75,12 @@ class HolidayControllerIT {
                 null,
                 null,
                 null
-        );
-        Preferences preferences = new Preferences(null, List.of(), user);
-        user.setPreferences(preferences);
-        userRepository.save(user);
+        ));
     }
 
     @Test
-    @WithMockUser(authorities = "SCOPE_employee", username = USER_ID)
-    void test_findAllByUser_succeeds() throws Exception {
-        Holiday holiday = createAndSaveHoliday(HolidayRequestStatus.REQUESTED);
-
+    @WithMockUser(authorities = "SCOPE_employee")
+    void test_getFindAllHolidaysByUser_succeeds() throws Exception {
         String response = mockMvc.perform(MockMvcRequestBuilders.get("/api/holiday"))
                 .andExpect(status().isOk())
                 .andReturn().getResponse().getContentAsString();
@@ -95,79 +89,100 @@ class HolidayControllerIT {
 
         assertAll(
                 () -> assertNotNull(holidays),
-                () -> assertEquals(1, holidays.size()),
-                () -> assertEquals(holiday.getId(), holidays.get(0).id()),
-                () -> assertEquals(user.getId(), holidays.get(0).user())
+                () -> assertEquals(0, holidays.size())
         );
-    }
-
-    @Test
-    @WithMockUser(authorities = "SCOPE_employee", username = USER_ID)
-    void test_findByIdAndUser_succeeds() throws Exception {
-        Holiday holiday = createAndSaveHoliday(HolidayRequestStatus.REQUESTED);
-
-        String response = mockMvc.perform(MockMvcRequestBuilders.get("/api/holiday/" + holiday.getId()))
-                .andExpect(status().isOk())
-                .andReturn().getResponse().getContentAsString();
-        HolidayDto holidayDto = objectMapper.readValue(response, HolidayDto.class);
-
-        assertAll(
-                () -> assertNotNull(holidayDto),
-                () -> assertEquals(holiday.getId(), holidayDto.id()),
-                () -> assertEquals(holiday.getStartDate(), holidayDto.startDate()),
-                () -> assertEquals(holiday.getEndDate(), holidayDto.endDate()),
-                () -> assertEquals(holiday.getStatus().name(), holidayDto.status()),
-                () -> assertEquals(holiday.getUser().getId(), holidayDto.user())
-        );
-    }
-
-    @Test
-    @WithMockUser(authorities = "SCOPE_dm", username = USER_ID)
-    void test_findAllByTeam_succeeds() throws Exception {
-        Holiday holiday = createAndSaveHoliday(HolidayRequestStatus.REQUESTED);
-
-        String response = mockMvc.perform(MockMvcRequestBuilders.get("/api/holiday/team"))
-                .andExpect(status().isOk())
-                .andReturn().getResponse().getContentAsString();
-        List<HolidayDto> holidays = objectMapper.readValue(response, new TypeReference<>() {
-        });
-
-        assertAll(
-                () -> assertNotNull(holidays),
-                () -> assertEquals(1, holidays.size()),
-                () -> assertEquals(holiday.getId(), holidays.get(0).id())
-        );
-    }
-
-    @Test
-    @WithMockUser(authorities = "SCOPE_employee", username = USER_ID)
-    void test_findAllByTeam_withEmployee_fails() throws Exception {
-         mockMvc.perform(MockMvcRequestBuilders.get("/api/holiday/team"))
-                .andExpect(status().is4xxClientError());
     }
 
     @Test
     @WithMockUser(authorities = "SCOPE_admin")
-    void test_findAll_succeeds() throws Exception {
-        Holiday holiday = createAndSaveHoliday(HolidayRequestStatus.REQUESTED);
+    void test_deleteHoliday_succeeds() throws Exception {
+        Holiday holiday = new Holiday();
+        holiday.setStartDate(LocalDate.now().plusDays(5));
+        holiday.setEndDate(LocalDate.now().plusDays(10));
+        holiday.setStatus(HolidayRequestStatus.REQUESTED);
+        holiday.setUser(user);
+        Holiday savedHoliday = holidayRepository.save(holiday);
 
-        String response = mockMvc.perform(MockMvcRequestBuilders.get("/api/holiday/all"))
-                .andExpect(status().isOk())
-                .andReturn().getResponse().getContentAsString();
-        List<HolidayDto> holidays = objectMapper.readValue(response, new TypeReference<>() {
-        });
+        mockMvc.perform(MockMvcRequestBuilders.delete("/api/holiday/" + savedHoliday.getId()))
+                .andExpect(status().isNoContent());
 
         assertAll(
-                () -> assertNotNull(holidays),
-                () -> assertEquals(1, holidays.size()),
-                () -> assertEquals(holiday.getId(), holidays.get(0).id())
+                () -> assertFalse(holidayRepository.existsById(savedHoliday.getId())),
+                () -> assertEquals(0, holidayRepository.count())
+        );
+    }
+
+    @Test
+    @WithMockUser(authorities = "SCOPE_employee")
+    void test_findHolidayByUserAndId_succeeds() throws Exception {
+        Holiday holiday = new Holiday();
+        holiday.setStartDate(LocalDate.now().plusDays(5));
+        holiday.setEndDate(LocalDate.now().plusDays(10));
+        holiday.setStatus(HolidayRequestStatus.REQUESTED);
+        holiday.setUser(user);
+        Holiday savedHoliday = holidayRepository.save(holiday);
+
+        String response = mockMvc.perform(MockMvcRequestBuilders.get("/api/holiday/" + savedHoliday.getId()))
+                .andExpect(status().isOk())
+                .andReturn().getResponse().getContentAsString();
+
+        HolidayDto foundHoliday = objectMapper.readValue(response, HolidayDto.class);
+
+        assertAll(
+                () -> assertEquals(savedHoliday.getId(), foundHoliday.id()),
+                () -> assertEquals(holiday.getStartDate(), foundHoliday.startDate()),
+                () -> assertEquals(holiday.getEndDate(), foundHoliday.endDate()),
+                () -> assertEquals(holiday.getStatus().name(), foundHoliday.status()),
+                () -> assertEquals(holiday.getUser().getId(), foundHoliday.user())
+        );
+    }
+
+    @Test
+    @WithMockUser(authorities = "SCOPE_employee")
+    void test_updateHoliday_succeeds() throws Exception {
+        Holiday holiday = new Holiday();
+        holiday.setStartDate(LocalDate.now().plusDays(5));
+        holiday.setEndDate(LocalDate.now().plusDays(10));
+        holiday.setStatus(HolidayRequestStatus.REQUESTED);
+        holiday.setUser(user);
+        Holiday savedHoliday = holidayRepository.save(holiday);
+
+        HolidayDto updatedHolidayDto = new HolidayDto(
+                savedHoliday.getId(),
+                LocalDate.now().plusDays(5),
+                LocalDate.now().plusDays(11),
+                null,
+                user.getId()
+        );
+
+        String response = mockMvc.perform(MockMvcRequestBuilders.put("/api/holiday")
+                        .contentType("application/json")
+                        .content(objectMapper.writeValueAsString(updatedHolidayDto)))
+                .andExpect(status().isOk())
+                .andReturn().getResponse().getContentAsString();
+
+        HolidayDto responseHoliday = objectMapper.readValue(response, HolidayDto.class);
+
+        assertAll(
+                () -> assertEquals(savedHoliday.getId(), responseHoliday.id()),
+                () -> assertEquals(updatedHolidayDto.startDate(), responseHoliday.startDate()),
+                () -> assertEquals(updatedHolidayDto.endDate(), responseHoliday.endDate()),
+                () -> assertEquals(updatedHolidayDto.status(), responseHoliday.status()),
+                () -> assertEquals(updatedHolidayDto.user(), responseHoliday.user()),
+                () -> assertEquals(1, holidayRepository.count())
         );
     }
 
     @Test
     @WithMockUser(authorities = "SCOPE_employee", username = USER_ID)
     void test_createHoliday_succeeds() throws Exception {
-        HolidayDto dto = getHolidayDto(null, LocalDate.now().plusDays(5), LocalDate.now().plusDays(10));
+        HolidayDto dto = new HolidayDto(
+                null,
+                LocalDate.now().plusDays(5),
+                LocalDate.now().plusDays(10),
+                null,
+                user.getId()
+        );
 
         String response = mockMvc.perform(
                         MockMvcRequestBuilders.post("/api/holiday")
@@ -183,119 +198,9 @@ class HolidayControllerIT {
                 () -> assertNotNull(created.id()),
                 () -> assertEquals(dto.startDate(), created.startDate()),
                 () -> assertEquals(dto.endDate(), created.endDate()),
-                () -> assertEquals(dto.status(), created.status()),
-                () -> assertEquals(USER_ID, created.user().toString()),
+                () -> assertEquals(HolidayRequestStatus.REQUESTED.name(), created.status()),
+                () -> assertEquals(dto.user(), created.user()),
                 () -> assertEquals(1, holidayRepository.count())
-        );
-    }
-
-    @Test
-    @WithMockUser(authorities = "SCOPE_admin")
-    void test_delete_succeeds() throws Exception {
-        Holiday holiday = createAndSaveHoliday(HolidayRequestStatus.REJECTED);
-
-        assertAll(
-                () -> assertEquals(1, holidayRepository.count()),
-                () -> assertTrue(holidayRepository.existsById(holiday.getId()))
-        );
-
-        mockMvc.perform(MockMvcRequestBuilders.delete("/api/holiday/" + holiday.getId()))
-                .andExpect(status().isNoContent());
-
-        assertAll(
-                () -> assertFalse(holidayRepository.existsById(holiday.getId())),
-                () -> assertEquals(0, holidayRepository.count())
-        );
-    }
-
-    @Test
-    @WithMockUser(authorities = "SCOPE_admin")
-    void test_delete_whenStatusApproved_fails() throws Exception {
-        Holiday holiday = createAndSaveHoliday(HolidayRequestStatus.APPROVED);
-
-        assertAll(
-                () -> assertEquals(1, holidayRepository.count()),
-                () -> assertTrue(holidayRepository.existsById(holiday.getId()))
-        );
-
-        mockMvc.perform(MockMvcRequestBuilders.delete("/api/holiday/" + holiday.getId()))
-                .andExpect(status().is4xxClientError());
-
-        assertAll(
-                () -> assertTrue(holidayRepository.existsById(holiday.getId())),
-                () -> assertEquals(1, holidayRepository.count())
-        );
-    }
-
-    @Test
-    @WithMockUser(authorities = "SCOPE_employee", username = USER_ID)
-    void test_updateHoliday_succeeds() throws Exception {
-        Holiday holiday = createAndSaveHoliday(HolidayRequestStatus.APPROVED);
-
-        HolidayDto holidayDto = getHolidayDto(holiday.getId(), LocalDate.now().plusDays(6),
-                LocalDate.now().plusDays(10));
-
-        String response = mockMvc.perform(MockMvcRequestBuilders.put("/api/holiday")
-                        .contentType("application/json")
-                        .content(objectMapper.writeValueAsString(holidayDto)))
-                .andExpect(status().isOk())
-                .andReturn().getResponse().getContentAsString();
-
-        HolidayDto responseHoliday = objectMapper.readValue(response, HolidayDto.class);
-
-        assertAll(
-                () -> assertEquals(holiday.getId(), responseHoliday.id()),
-                () -> assertEquals(holidayDto.startDate(), responseHoliday.startDate()),
-                () -> assertEquals(holidayDto.endDate(), responseHoliday.endDate()),
-                () -> assertEquals(HolidayRequestStatus.REQUESTED.name(), responseHoliday.status()),
-                () -> assertEquals(holiday.getUser().getId(), responseHoliday.user()),
-                () -> assertEquals(1, holidayRepository.count())
-        );
-    }
-
-    @Test
-    @WithMockUser(authorities = "SCOPE_dm", username = USER_ID)
-    void test_updateStatus_succeeds() throws Exception {
-        Holiday holiday = createAndSaveHoliday(HolidayRequestStatus.REQUESTED);
-
-        String response = mockMvc.perform(MockMvcRequestBuilders.put("/api/holiday/" + holiday.getId()
-                        + "/" + HolidayRequestStatus.APPROVED.name()))
-                .andExpect(status().isOk())
-                .andReturn().getResponse().getContentAsString();
-        HolidayDto updatedHoliday = objectMapper.readValue(response, HolidayDto.class);
-
-        assertAll(
-                () -> assertNotNull(updatedHoliday),
-                () -> assertEquals(holiday.getId(), updatedHoliday.id()),
-                () -> assertEquals(HolidayRequestStatus.APPROVED.name(), updatedHoliday.status())
-        );
-    }
-
-    @Test
-    @WithMockUser(authorities = "SCOPE_employee", username = USER_ID)
-    void test_updateStatus_fails() throws Exception {
-        Holiday holiday = createAndSaveHoliday(HolidayRequestStatus.REQUESTED);
-        mockMvc.perform(MockMvcRequestBuilders.put("/api/holiday/" + holiday.getId()
-                        + "/" + HolidayRequestStatus.APPROVED.name()))
-                .andExpect(status().is4xxClientError());
-    }
-
-    private Holiday createAndSaveHoliday(HolidayRequestStatus status) {
-        Holiday holiday = new Holiday();
-        holiday.setStartDate(LocalDate.now().plusDays(5));
-        holiday.setEndDate(LocalDate.now().plusDays(10));
-        holiday.setUser(user);
-        holiday.setStatus(Objects.requireNonNullElse(status, HolidayRequestStatus.REQUESTED));
-        return holidayRepository.save(holiday);
-    }
-
-    private HolidayDto getHolidayDto(UUID id, LocalDate startDate, LocalDate endDate) {
-        return new HolidayDto(
-                id,
-                startDate,
-                endDate,
-                null,
-                null
         );
     }
 }
